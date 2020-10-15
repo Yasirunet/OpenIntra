4d180b7a253d75ace89c401c5ea91f9443077e34
yasiru <yasirunet@gmail.com>
2020-10-15 12:17:07 +0530

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	deleted:    src/App.js 	new file:   src/components/app/App.js 	new file:   src/components/app/index.js 	modified:   src/index.js 	new file:   src/routes/home/home.js 	new file:   src/routes/home/index.js
    Following are the changes made with this commit:
    - setup proper file structure
    - moved App.js to component folder
    - installed material-ui and material-ui/icons libraries

---
+++ src/components/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from '../../routes/home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/home/home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={{ textAlign: 'center', margin: '100px' }}>
+      <h1 style={{ padding: '20px' }}>OpenIntra</h1>
+      <h4>
+        Welcome to the OpenIntra.Here is the very begining of the project.
+      </h4>
+      <p>
+        To getstarted to contribute, please view{' '}
+        <a href="https://github.com/Yasirunet/OpenIntra/issues">issues</a>
+      </p>
+    </div>
+  );
+}
+
+export default Home;
