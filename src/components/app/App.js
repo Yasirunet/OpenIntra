3161f41a41a88c612168a75154c3b99bc35dea12
yasiru <yasirunet@gmail.com>
2020-10-16 21:31:33 +0530

    setup navbar component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Home from '../../routes/home';
+import NavBar from '../navbar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Home />
     </div>
   );
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { useStyles } from './navbar.style';
+
+function NavBar() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="inherit">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            OpenIntra
+          </Typography>
+          <Button color="secondary">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default NavBar;
