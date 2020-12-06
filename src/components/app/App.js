d439fb220e521f559134655635c62978deb1ee45
Samadhi Wathsala <samadhiwathsala96@gmail.com>
2020-12-07 02:39:13 +0530

    signIn and account request form create

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import Home from '../../routes/home';
-import NavBar from '../navbar';
+import {Route, BrowserRouter as Router , Switch} from 'react-router-dom'
+// import Home from '../../routes/home';
+// import NavBar from '../navbar';
+import SignIn from '../../routes/authentication/signIn'
+import SignUp from '../../routes/authentication/signUp'
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Home />
+      {/* <NavBar />
+      <Home /> */}
+      <Router>
+        <Switch>
+          <Route path="/" exact component= {SignIn} />
+          <Route path="/signUp" component= {SignUp} />
+        </Switch>
+      </Router>
     </div>
   );
 }
